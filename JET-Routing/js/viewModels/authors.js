3aeb2a9aa3e8d1c48eb1357df685e8cad1e9ff03
peppertech <peppertech@gmail.com>
2016-04-26 09:17:29 -0700

    adding router lifecycle methods fixing bad path for overrides.css file reference

--- JET-Routing/js/viewModels/authors.js
+++ JET-Routing/js/viewModels/authors.js
@@ -8,13 +8,29 @@ define(['ojs/ojcore', 'knockout'
      */
     function authorsContentViewModel() {
         var self = this;
-        
-        self.handleActivated = function(){
+
+        self.handleActivated = function () {
+            //Change the page title as the new module is loaded so that 
+            //Assistive technology can read the proper page title out
             document.title = "ojRouter - Authors";
         };
-        
+
+        self.canEnter = function () {
+            // Use this router lifecycle method to determine if the page can be
+            // entered. This could be a place to check authorization to access the
+            // page and deny entry if not authorized. Return true or false.
+            return true;
+        };
+
+
+        self.canExit = function () {
+            // Use this router lifecycle method to tell the router
+            // if it's ok to leave this page. As an example, you may want to stop
+            // the user from leaving the page if there is a form that hasn't been saved.
+            return true;
+        };
     }
-    
+
     return authorsContentViewModel;
 });
 
--- JET-Routing/js/viewModels/collections.js
+++ JET-Routing/js/viewModels/collections.js
@@ -9,10 +9,28 @@ define(['ojs/ojcore', 'knockout'
     function collectionsContentViewModel() {
         var self = this;
 
-        self.handleActivated = function() {
+        self.handleActivated = function () {
+            //Change the page title as the new module is loaded so that 
+            //Assistive technology can read the proper page title out
             document.title = "ojRouter - Collections";
         };
 
+        self.canEnter = function () {
+            // Use this router lifecycle method to determine if the page can be
+            // entered. This could be a place to check authorization to access the
+            // page and deny entry if not authorized. Return true or false.
+            console.log('yes, you may Enter');
+            return true;
+        };
+
+
+        self.canExit = function () {
+            // Use this router lifecycle method to tell the router
+            // if it's ok to leave this page. As an example, you may want to stop
+            // the user from leaving the page if there is a form that hasn't been saved.
+            console.log('yes, you may Exit');
+            return true;
+        };
 
     }
 
