ed74e3b9b4a8e7079c55c40111f6b64d0f02a8c7
peppertech <peppertech@gmail.com>
2016-04-28 10:56:28 -0700

    adding layout control for books page. Can change between list and card layouts now

--- JET-Routing/js/viewModels/books.js
+++ JET-Routing/js/viewModels/books.js
@@ -1,11 +1,31 @@
 /**
  * books module
  */
-define(['ojs/ojcore', 'knockout'
-], function (oj, ko) {
+define(['ojs/ojcore', 'knockout','ojs/ojbutton'], 
+function (oj, ko) {
     /**
      * The view model for the main content view template
      */
+
+//    var parentRouter = params.ojRouter.parentRouter;
+//
+//    this.router = parentRouter.createChildRouter('layout')
+//            .configure(
+//                    {
+//                        'card': {label: 'Card', isDefault: true},
+//                        'list': {label: 'List'}
+//                    });
+
+//    function mergeConfig(original)
+//    {
+//        return $.extend(true, {}, original,
+//                {
+//                    'params': {'data': empArray}
+//                });
+//    }
+//
+//    this.moduleConfig = mergeConfig(this.router.moduleConfig);
+
     function booksContentViewModel() {
         var self = this;
         self.books = ko.observableArray([]);
@@ -17,15 +37,12 @@ define(['ojs/ojcore', 'knockout'
             document.title = "ojRouter - Books";
         };
 
-        self.allData = $.getJSON('js/data/productData.json').then(function(data){
-            $.each(data, function(idx, val){
+        self.allData = $.getJSON('js/data/productData.json').then(function (data) {
+            $.each(data, function (idx, val) {
                 self.books().push({"title": val.TITLE});
             });
             self.books.valueHasMutated();
         });
-
-
-
 
         self.canExit = function () {
             // Use this router lifecycle method to tell the router
